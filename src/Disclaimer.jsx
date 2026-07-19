4c3c144ee718b3764230e9a895bf6b7139c1adb0
JimenaMV21 <jimena.moralesvi@anahuac.mx>
2026-07-19 00:17:41 -0600

    Se arreglo correspodencia

--- src/Disclaimer.jsx
+++ src/Disclaimer.jsx
@@ -13,9 +13,17 @@ export default function Disclaimer() {
         maxWidth: '800px', 
         margin: '0 auto' 
       }}>
-        <div style={{ flex: '1', minWidth: '250px' }}>
-          <h4 style={{ margin: '0 0 10px 0' }}>Aviso Legal</h4>
-          <p style={{ fontSize: '0.9rem', color: '#555' }}>
+        <div style={{ 
+  display: 'flex', 
+  flexWrap: 'wrap', 
+  padding: '10px', 
+  gap: '5px', 
+  maxWidth: '500px',
+  margin: '0 auto' 
+}}>
+          <h4 style={{ margin: '0 0 10px 0', justifyContent:'center',  flex: '2', minWidth: '250px', color: '#7bb588' }}>Aviso Legal</h4>
+          
+          <p style={{ fontSize: '0.9rem', color: '#555', textAlign: 'justify' }}>
             Esta herramienta tiene fines exclusivamente informativos. 
   No constituye un programa médico ni sustituye el diagnóstico, tratamiento o consejo de un profesional de la salud. 
    Por favor, consulte a un médico ante cualquier síntoma, duda o preocupación sobre su estado de salud
@@ -23,8 +31,8 @@ export default function Disclaimer() {
           </p>
         </div>
         
-        <div style={{ flex: '1', minWidth: '250px' }}>
-          <h4 style={{ margin: '0 0 10px 0' }}>Correspondecia</h4>
+        <div style={{ flex: '2', minWidth: '250px' }}>
+          <h4 style={{ margin: '0 0 10px 0', color: '#7bb588' }}>Correspondecia</h4>
 
           <p style={{ fontSize: '0.9rem', color: '#555' }}>
             Carlos Mauricio Lastre Dominguez
@@ -45,8 +53,6 @@ export default function Disclaimer() {
       </div>
 
       <br />
-
-      {/* El disclaimer legal debajo */}
       
     </footer>
   );
