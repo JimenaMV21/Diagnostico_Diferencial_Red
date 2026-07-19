b55c7710eb4f00b667ba6f03015350cd014a1fd2
JimenaMV21 <jimena.moralesvi@anahuac.mx>
2026-07-19 01:17:36 -0600

    Error 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Disclaimer from './Disclaimer';
 // importacion imagenes
 import logoTec from './Imagenes/logo-Tec.jpeg';
 import logoTecT from './Imagenes/logo-TecT.jpeg';
-import logoSecihti from './Imagenes/Logo-SE.jpeg';
+import logoSecihti from './Imagenes/logo-SE.jpeg';
 import logoUao from './Imagenes/logo-uao.jpg';
 import logoUabjo from './Imagenes/logo-Uabjo.jpeg';
 
